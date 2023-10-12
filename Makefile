cf79263d0ef93a00825e008af75aa204f187d928
heghine <heghine.balasanyan@gmail.com>
2023-10-12 17:56:10 +0400

    upd: minishell init

---
+++ Makefile
@@ -0,0 +1,39 @@
+NAME			= minishell
+
+SRC_DIR 		= src
+SRCS			= $(wildcard $(SRC_DIR)/*.c)
+HEADERS			= $(wildcard includes/*.h)
+OBJ_DIR			= obj
+OBJS			= $(patsubst $(SRC_DIR)/%.c, $(OBJ_DIR)/%.o, $(SRCS))
+INCS			= -Iincludes
+CC				= cc 
+RM				= rm -rf
+CFLAGS			= -Wall -Wextra -Werror #-fsanitize=address -g
+MK				= mkdir -p
+
+LIBFT			= ./libraries/libft/libft.a
+LIBFT_PATH		= ./libraries/libft
+
+all:			$(OBJ_DIR) $(NAME)
+
+$(OBJ_DIR): $(SRC_DIR)
+				@$(MK) $(OBJ_DIR)
+
+$(OBJ_DIR)/%.o:	$(SRC_DIR)/%.c $(HEADERS)
+				$(CC) $(CFLAGS) $(INCS) -c $< -o $@
+
+$(NAME):		$(OBJS)
+				@$(MAKE) -C ${LIBFT_PATH} > /dev/null 2>&1
+				@cp ${LIBFT} $(NAME)
+				@$(CC) $(CFLAGS) $(INCS) -o $(NAME) $(OBJS)
+
+clean:			
+				@$(MAKE) clean -C ${LIBFT_PATH}  > /dev/null 2>&1
+				$(RM) $(OBJ_DIR)
+
+fclean:			clean
+				$(RM) $(NAME)
+				
+re:				fclean all
+
+.PHONY:			all clean fclean re
---
+++ libraries/libft/ft_strjoin.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbalasan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/20 14:23:26 by hbalasan          #+#    #+#             */
+/*   Updated: 2023/02/21 20:51:32 by hbalasan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		i;
+	int		j;
+
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		*(str + i) = *(s1 + i);
+		i++;
+	}
+	while (s2[j])
+	{
+		*(str + i + j) = *(s2 + j);
+		j++;
+	}
+	*(str + i + j) = '\0';
+	return (str);
+}
