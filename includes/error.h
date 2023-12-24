b2278e095ba15eb57ef737c0e3ab7125e6c1a741
Tigran Mnatsakanyan <tg.mnatsakanyan@mail.ru>
2023-12-24 18:22:52 +0400

    fix norminette

--- includes/error.h
+++ includes/error.h
@@ -3,34 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   error.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hbalasan <hbalasan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tmnatsak <tmnatsak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 01:53:08 by hbalasan          #+#    #+#             */
-/*   Updated: 2023/12/22 20:27:14 by hbalasan         ###   ########.fr       */
+/*   Updated: 2023/12/24 18:18:51 by tmnatsak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERROR_H
 # define ERROR_H
 
-typedef enum s_error
-{
-	EPERM = 1, // permission denied: 126
-	EMEM, // no memory left on device 
-	NOTDIR, // Not a directory: 
-	ISDIR, // Is a directory: 
-	NDIR, // No such file or directory: 
-	ECMD, // command not found: 127
-	EFORK, // fork failed
-	EPIPE, // error creating pipe
-	EPIPEND, // syntax error near unexpected token `|'
-	DUPERR, // dup2 failed
-	EQUOTE // error while looking for matching quote
-}		t_error;
-
 void	error_msg(char *msg);
 void	*mini_error(int errmsg, char *param, int errcode);
 void	print_error_msg_fd(char *msg, int nl);
 void	mini_cd_error(char **full_cmd, char **str);
+
+typedef enum s_error
+{
+	EPERM = 1,
+	EMEM,
+	NOTDIR,
+	ISDIR,
+	NDIR,
+	ECMD,
+	EFORK,
+	EPIPE,
+	EPIPEND,
+	DUPERR,
+	EQUOTE
+}	t_error;
 
 #endif
